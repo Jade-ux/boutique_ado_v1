9cc9800f870ad13e952075655a3f70973ba5bb48
Jade-ux <jadeemailsandbox@gmail.com>
2021-04-03 16:46:57 +0000

    Fixed syntax error in settings file.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -117,9 +117,9 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-if 'DATABASE_URL' is in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
